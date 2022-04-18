d9d85982e922752873d78754dcc26556479dde6f
Delfino Farfan <delfino.farfan18@gmail.com>
2022-04-18 18:59:21 -0500

    Function Declerations

---
+++ week3/3.1/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,35 @@
+// TODO: using the function expressions below, refactor them into function declarations
+const greet = function() {
+    console.log("Hello!");
+};
+
+function greet() {
+    console.log("Hello!");
+}
+greet();
+
+const threeModTwo = function() {
+    console.log(3 % 2);
+};
+
+function threeModTwo() {
+    console.log(3 % 2);
+}
+threeModTwo();
+
+let age = 18;
+const checkDrivingAge = function() {
+    if (age >= 16) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  };
+
+function checkDrivingAge() {
+    if (age >= 16) {
+        console.log(true);
+      } else {
+        console.log(false);
+      }
+}
